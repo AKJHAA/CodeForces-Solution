6e01da74e7df3ec380f7b14568ed6b410cfea8ac
Abhijeet Kumar <akjhaa1508@gmail.com>
2024-03-15 03:04:55 +0530

    House Robber II Added DP - 6

---
+++ LeetCode/HouseRobber2.java
@@ -0,0 +1,119 @@
+/*
+213. House Robber II
+
+You are a professional robber planning to rob houses along a street. 
+Each house has a certain amount of money stashed. All houses at this place are 
+arranged in a circle. That means the first house is the neighbor of the last one. 
+Meanwhile, adjacent houses have a security system connected, and it will automatically
+contact the police if two adjacent houses were broken into on the same night.
+Given an integer array nums representing the amount of money of each house, 
+return the maximum amount of money you can rob tonight without alerting the police.
+
+Example 1:
+
+Input: nums = [2,3,2]
+Output: 3
+Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2), because they are adjacent houses */
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n == 1) return nums[0];
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        for(int i = 0;i < n;i++){
+            if(i != 0)
+                list1.add(nums[i]);
+            if(i != n - 1)
+                list2.add(nums[i]);
+        }
+        return Math.max(HouseRobber1(list1) , HouseRobber1(list2));
+    }
+    public static int HouseRobber1(ArrayList<Integer> nums) {
+		// Write your code here.
+        int n = nums.size();
+        int prev = nums.get(0);
+        int prev2 = 0;
+        for(int i = 1 ;i < n;i++){
+            int pick = nums.get(i);
+            if(i > 1)
+                pick = pick + prev2;
+            int nonPick = prev;
+            int curr = Math.max(pick , nonPick);
+
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+}
+
+// Space Optimal Approach T.C-> O(N) , S.C-> O(1)
+    // public int rob(int[] nums) {
+    //     int n = nums.length;
+    //     int prev = nums[0];
+    //     int prev2 = 0;
+    //     for(int i = 1 ;i < n;i++){
+    //         int pick = nums[i];
+    //         if(i > 1)
+    //             pick = pick + prev2;
+    //         int nonPick = prev;
+    //         int curr = Math.max(pick , nonPick);
+
+    //         prev2 = prev;
+    //         prev = curr;
+    //     }
+    //     return prev;
+    // }
+    /*
+    // --------------------TABULTAION APPROACH-----------------------
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int dp[] = new int[n];
+        dp[0] = nums[0];
+        for(int i = 1 ;i < n;i++){
+            int pick = nums[i];
+            if(i > 1)
+                pick = pick + dp[i - 2];
+            int nonPick = 0 + dp[i - 1];
+            dp[i] = Math.max(pick , nonPick);
+        }
+        return dp[n - 1];
+    }*/
+    /*
+    // MEMOIZATION
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int dp[] = new int[n];
+        Arrays.fill(dp , -1);
+        dp[0] = nums[0];
+        return memo(nums, n  - 1, dp);
+    }
+    public int memo(int nums[] , int ind , int dp[]){
+        if(ind == 0)
+            return dp[ind];
+        if(ind < 0)
+            return 0;
+        if(dp[ind] != -1)
+            return dp[ind];
+        int pick = nums[ind] + memo(nums , ind - 2 , dp);
+        int nonPic = 0 + memo(nums , ind - 1 , dp);
+
+        return dp[ind] = Math.max(pick , nonPic);
+    }*/
+    /*
+    // -----------TLE----------------
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int dp[] = new int[n];
+        return memo(nums, n  - 1, dp);
+    }
+    public int memo(int nums[] , int ind , int dp[]){
+        if(ind == 0)
+            return nums[ind];
+        if(ind < 0)
+            return 0;
+        int pick = nums[ind] + memo(nums , ind - 2 , dp);
+        int nonPic = 0 + memo(nums , ind - 1 , dp);
+
+        return Math.max(pick , nonPic);
+    }*/
